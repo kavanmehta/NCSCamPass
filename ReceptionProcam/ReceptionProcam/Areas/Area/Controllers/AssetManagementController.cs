a73fc751e4f2a298b7733d8a96194df54e8ec421
nilambari <nilambari.s123@gmail.com>
2018-12-28 11:59:12 +0530

    asset pages

---
+++ ReceptionProcam/ReceptionProcam/Areas/Area/Controllers/AssetManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReceptionProcam.Areas.Area.Controllers
+{
+    public class AssetManagementController : Controller
+    {
+        // GET: Area/AssetManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
