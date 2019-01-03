d95510dcd61f13e8256f1c76554a5296cfd70d10
nilambari <nilambari.s123@gmail.com>
2019-01-03 11:50:11 +0530

    asset details

--- ReceptionProcam/ReceptionProcam/Areas/Area/Controllers/AssetManagementController.cs
+++ ReceptionProcam/ReceptionProcam/Areas/Area/Controllers/AssetManagementController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReceptionProcam.EntityModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,50 @@ namespace ReceptionProcam.Areas.Area.Controllers
 {
     public class AssetManagementController : Controller
     {
+        DBNCSVisitorEntities objVisEnti = new DBNCSVisitorEntities();
         // GET: Area/AssetManagement
         [HttpGet]
         public ActionResult AssetDetails()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult LoadData()
+        {
+            try
+            {
+                //Creating instance of DatabaseContext class  
+
+                using (DBNCSVisitorEntities dc = new DBNCSVisitorEntities())
+                {
+                    var data = dc.uspGetVisitorDetailsNew().OrderBy(a => a.GovIdNo).ToList();
+                    return Json(new { data = data }, JsonRequestBehavior.AllowGet);
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        [HttpPost]
+        public Boolean SubmittAsset(string id)
+        {
+            var result = objVisEnti.tblVisitorVisitDetails.SingleOrDefault(b => b.GovIdNo == id);
+            if (result != null)
+            {
+                result.IsPassReturned = true;
+                objVisEnti.SaveChanges();
+                TempData["SuccessSubmit"] = "Asset Submitted successfully";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         [HttpGet]
         public ActionResult AssetIssue()
---
+++ ReceptionProcam/ReceptionProcam/Scripts/AssetDetails.js
@@ -0,0 +1,17 @@
+﻿function returnPass() {
+
+
+    var assetId = $("#hidSubmitAsset").val();
+    $.ajax({
+        type: "POST",
+        url: "/Areas/Area/AssetManagement/SubmitAsset?id=" + assetId,
+        dataType: "text",
+        success: function (data) {
+            $('#confirmModal').modal('toggle');
+            location.reload();
+        },
+        error: function () {
+            alert("Error occured!!")
+        }
+    });
+}
