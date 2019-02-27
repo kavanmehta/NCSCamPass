3f19935a48fc7e27c29ef0716ef1051a5b666995
nilambari <nilambari.s123@gmail.com>
2019-02-27 17:28:18 +0800

    model

---
+++ ReceptionProcam/ReceptionProcam/Areas/Area/Models/AssetAudits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReceptionProcam.Areas.Area.Models
+{
+    public class AssetAudits
+    {
+        public int ID {get;set;}
+        public int AssetIssueID { get; set; }
+        public int AuditYear { get; set; }
+        public string Remarks { get; set; }
+        public DateTime? AuditDate { get; set; }
+    }
+}
---
+++ ReceptionProcam/ReceptionProcam/Areas/Area/Models/EmployeeDesignations.cs
@@ -0,0 +1,18 @@
+﻿using ReceptionProcam.EntityModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReceptionProcam.Areas.Area.Models
+{
+    public class EmployeeDesignations
+    {
+        public int EmpDesignationID { get; set; }
+
+        public string EmpDesignationName { get; set; }
+    }
+}
